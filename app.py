cbcd058f1487b407df3da66a758109e6adf8c126
David <davidmoutinho1994@gmail.com>
2026-03-17 02:17:35 +0000

    feat(tasks): add task listing and retrieval endpoints

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
 from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
+
+tasks = []
+task_id_control = 1
+
+@app.route("/tasks", methods=["POST"])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id= task_id_control, title= data["title"], description= data.get("description", ""))
+    tasks.append(new_task)
+    task_id_control += 1
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+
+@app.route("/tasks", methods= ["GET"])
+def get_tasks():
+    tasks_list = [task.to_dict() for task in tasks]
+    output = {
+        "tasks": tasks_list,
+        "total_tasks": len(tasks_list)
+    }
+    return jsonify(output)
+
+@app.route("/tasks/<int:id>", methods= ["GET"])
+def get_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    return jsonify({"message": "Não foi possivel encontrar a tarefa"}), 404
 
 
 
