ad47021e955ef8e334a69e1cb3d1f6ea598b551e
David <davidmoutinho1994@gmail.com>
2026-04-16 22:15:31 +0000

    test(tasks): refine API tests and validate task deletion behavior

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import requests
 
 BASE_URL = 'http://127.0.0.1:5000'
 
-
+# Testando o endpoint CREATE TASK
 def test_create_task():
     new_task_data = {
         "title" : "teste",
@@ -15,7 +15,7 @@ def test_create_task():
     assert "message" in response_json
     assert "id" in response_json
 
-
+# Testando o endpoint GET TASKS
 def test_get_tasks():
     new_task_data = {
         "title" : "teste",
@@ -33,9 +33,8 @@ def test_get_tasks():
     assert "tasks" in response_get_json
     assert "total_tasks" in response_get_json
 
-
+# Testando o endpoint GET TASK
 def test_get_task():
-    tasks_test = []
     new_task_data = {
         "title" : "Teste",
         "description" : "Descrição"
@@ -45,18 +44,13 @@ def test_get_task():
     response_create_json = response_create.json()
     assert "message" in response_create_json
     assert "id" in response_create_json
-    tasks_test.append(response_create_json["id"])
 
-    if tasks_test:
-        task_id = tasks_test[0]
-    response_get = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    response_get = requests.get(f"{BASE_URL}/tasks/{response_create_json["id"]}")
     assert response_get.status_code == 200
     response_get_json = response_get.json()
-    assert task_id == response_get_json["id"]
 
-
+#Testando o endpoint UPDATE TASK
 def test_update_task():
-    tasks_test = []
     new_task_data = {
         "title" : "Teste",
         "description" : "Descrição"
@@ -66,29 +60,26 @@ def test_update_task():
     response_create_json = response_create.json()
     assert "message" in response_create_json
     assert "id" in response_create_json
-    tasks_test.append(response_create_json["id"])
 
     update_task_data = {
         "title" : "Atualizando a tarefa",
         "description" : "descrição",
         "completed" : True
     }
-    if tasks_test:
-        task_id = tasks_test[0]
-    response_update = requests.put(f"{BASE_URL}/tasks/{task_id}", json = update_task_data)
+    
+    response_update = requests.put(f"{BASE_URL}/tasks/{response_create_json["id"]}", json = update_task_data)
     assert response_update.status_code == 200
     response_update_json = response_update.json()
     assert "message" in  response_update_json
 
-    response_get = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    response_get = requests.get(f"{BASE_URL}/tasks/{response_create_json["id"]}")
     response_get_json = response_get.json()
     assert update_task_data["title"] == response_get_json["title"]
     assert update_task_data["description"] == response_get_json["description"]
     assert update_task_data["completed"] == response_get_json["completed"]
 
-
+# Testando o endpoint DELETE TASK
 def test_delete_task():
-    tasks_test = []
     new_task_data = {
         "title" : "titulo",
         "description" : "descrição"
@@ -98,17 +89,12 @@ def test_delete_task():
     response_create_json = response_create.json()
     assert "message" in response_create_json
     assert "id" in response_create_json
-    tasks_test.append(response_create_json["id"])
 
-    if tasks_test:
-        task_id = tasks_test[0]
-    response_delete = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+    
+    response_delete = requests.delete(f"{BASE_URL}/tasks/{response_create_json["id"]}")
     assert response_delete.status_code == 200
     response_delete_json = response_delete.json()
     assert "message" in response_delete_json
-
-    if not tasks_test:
-        response_get = requests.get(f"{BASE_URL}/tasks")
-        assert response_get.status_code == 200
-        response_get_json = response_get.json()
-        assert response_get_json["total_tasks"] == 0
+    
+    response_get_task = requests.get(f"{BASE_URL}/tasks/{response_create_json["id"]}")
+    assert response_get_task.status_code == 404
